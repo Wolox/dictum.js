62fe2c88ae9640e400f0025d23a42cd334acbe7f
Michel Agopian <mishuagopian@gmail.com>
2017-05-18 20:33:27 -0300

    moving markdown helpers to new file

--- test/markdownHelpers.js
+++ test/markdownHelpers.js
@@ -1,93 +1,93 @@
-var markdownWriter = require('./../utils/markdownWriter');
+var markdownHelpers = require('./../utils/markdownHelpers');
 
-describe('markdownWriter', function () {
+describe('markdownHelpers', function () {
   describe('.some titleMarkdown', function () {
     it('should return the correct # markdown', function () {
-      expect(markdownWriter.resourceMarkdown('resource')).to.be.equal('# Resource\n');
+      expect(markdownHelpers.resourceMarkdown('resource')).to.be.equal('# Resource\n');
     });
 
     it('should return an empty string markdown when providing no data', function () {
-      expect(markdownWriter.resourceMarkdown()).to.be.equal('');
+      expect(markdownHelpers.resourceMarkdown()).to.be.equal('');
     });
   });
 
   describe('.endpointMarkdown', function () {
     it('should return the correct ## markdown when providing method and endpoint', function () {
-      expect(markdownWriter.endpointMarkdown('get', '/books')).to.be.equal('## GET /books\n');
+      expect(markdownHelpers.endpointMarkdown('get', '/books')).to.be.equal('## GET /books\n');
     });
 
     it('should return an empty string when providing only method', function () {
-      expect(markdownWriter.endpointMarkdown('get')).to.be.equal('');
+      expect(markdownHelpers.endpointMarkdown('get')).to.be.equal('');
     });
 
     it('should return an empty string markdown when providing only endpoint', function () {
-      expect(markdownWriter.endpointMarkdown(null, '/books')).to.be.equal('');
+      expect(markdownHelpers.endpointMarkdown(null, '/books')).to.be.equal('');
     });
 
     it('should return an empty string markdown when providing no data', function () {
-      expect(markdownWriter.endpointMarkdown()).to.be.equal('');
+      expect(markdownHelpers.endpointMarkdown()).to.be.equal('');
     });
   });
 
   describe('.endpointDescriptionMarkdown', function () {
     it('should return the correct description with new line at the end', function () {
-      expect(markdownWriter.endpointDescriptionMarkdown('some description')).to.be.equal('some description\n');
+      expect(markdownHelpers.endpointDescriptionMarkdown('some description')).to.be.equal('some description\n');
     });
 
     it('should return an empty string markdown when providing no data', function () {
-      expect(markdownWriter.endpointDescriptionMarkdown()).to.be.equal('');
+      expect(markdownHelpers.endpointDescriptionMarkdown()).to.be.equal('');
     });
   });
 
   describe('.dataTitleMarkdown', function () {
     it('should return the correct ### markdown', function () {
-      expect(markdownWriter.dataTitleMarkdown('some title')).to.be.equal('### Some title\n');
+      expect(markdownHelpers.dataTitleMarkdown('some title')).to.be.equal('### Some title\n');
     });
 
     it('should return an empty string markdown when providing no data', function () {
-      expect(markdownWriter.dataTitleMarkdown()).to.be.equal('');
+      expect(markdownHelpers.dataTitleMarkdown()).to.be.equal('');
     });
   });
 
   describe('.dataSubtitleMarkdown', function () {
     it('should return the correct #### markdown', function () {
-      expect(markdownWriter.dataSubtitleMarkdown('some subtitle')).to.be.equal('#### Some subtitle\n');
+      expect(markdownHelpers.dataSubtitleMarkdown('some subtitle')).to.be.equal('#### Some subtitle\n');
     });
 
     it('should return an empty string markdown when providing no data', function () {
-      expect(markdownWriter.dataSubtitleMarkdown()).to.be.equal('');
+      expect(markdownHelpers.dataSubtitleMarkdown()).to.be.equal('');
     });
   });
 
   describe('.jsonMarkdown', function () {
     it('should return the correct formatted json markdown', function () {
       var obj = { key: 'value'};
-      expect(markdownWriter.jsonMarkdown(obj)).to.be.equal('```json\n' + JSON.stringify(obj, null, 2) + '\n```\n');
+      expect(markdownHelpers.jsonMarkdown(obj)).to.be.equal('```json\n' + JSON.stringify(obj, null, 2) + '\n```\n');
     });
 
     it('should return an empty string markdown when providing no data', function () {
-      expect(markdownWriter.jsonMarkdown()).to.be.equal('');
+      expect(markdownHelpers.jsonMarkdown()).to.be.equal('');
     });
   });
 
   describe('.titleJsonMarkdown', function () {
     it('should return the correct ### markdown with the correct formatted json when title and object provided', function () {
       var obj = { key: 'value'};
-      expect(markdownWriter.titleJsonMarkdown('some title', obj)).to.be.equal(
+      expect(markdownHelpers.titleJsonMarkdown('some title', obj)).to.be.equal(
         '#### Some title\n' + '```json\n' + JSON.stringify(obj, null, 2) + '\n```\n'
       );
     });
 
     it('should return an empty string when only providing title', function () {
-      expect(markdownWriter.titleJsonMarkdown('some title')).to.be.equal('');
+      expect(markdownHelpers.titleJsonMarkdown('some title')).to.be.equal('');
     });
 
     it('should return an empty string when only providing an object', function () {
-      expect(markdownWriter.titleJsonMarkdown(null, { key: 'some value' })).to.be.equal('');
+      expect(markdownHelpers.titleJsonMarkdown(null, { key: 'some value' })).to.be.equal('');
     });
 
     it('should return empty an string markdown when providing no data', function () {
-      expect(markdownWriter.titleJsonMarkdown('some title')).to.be.equal('');
+      expect(markdownHelpers.titleJsonMarkdown('some title')).to.be.equal('');
     });
   });
 });
--- utils/markdownWriter.js
+++ utils/markdownWriter.js
@@ -1,74 +1,33 @@
-var fs = require('fs');
+var fs = require('fs'),
+  markdownHelpers = require('./markdownHelpers');
 
 exports.generate = function (data) {
-  var data = exports.generateData(data);
-  if (data) {
-    fs.writeFileSync('./docs/docs.md', data);
-  }
-};
-
-exports.generateData = function (data) {
   var resources = data.resources;
-  var data = [];
 
   if (resources) {
     var resourcesKeys = Object.keys(resources);
+    var data = [];
 
     resourcesKeys.forEach(function (resourceKey) {
       var endpoints = resources[resourceKey].endpoints;
 
-      data.push(exports.resourceMarkdown(resourceKey));
+      data.push(markdownHelpers.resourceMarkdown(resourceKey));
 
       endpoints.forEach(function (endpoint) {
-        data.push(exports.endpointMarkdown(endpoint.method, endpoint.endpoint));
-        data.push(exports.endpointDescriptionMarkdown(endpoint.description));
+        data.push(markdownHelpers.endpointMarkdown(endpoint.method, endpoint.endpoint));
+        data.push(markdownHelpers.endpointDescriptionMarkdown(endpoint.description));
 
-        data.push(exports.dataTitleMarkdown('Request data'));
-        data.push(exports.titleJsonMarkdown('Headers', endpoint.requestHeaders));
-        data.push(exports.titleJsonMarkdown('Body', endpoint.requestBodyParams));
+        data.push(markdownHelpers.dataTitleMarkdown('Request data'));
+        data.push(markdownHelpers.titleJsonMarkdown('Headers', endpoint.requestHeaders));
+        data.push(markdownHelpers.titleJsonMarkdown('Body', endpoint.requestBodyParams));
 
-        data.push(exports.dataTitleMarkdown('Response data'));
-        data.push(exports.dataSubtitleMarkdown('Status: ' + endpoint.responseStatus));
-        data.push(exports.titleJsonMarkdown('Headers', endpoint.responseHeaders));
-        data.push(exports.titleJsonMarkdown('Body', endpoint.responseBody));
+        data.push(markdownHelpers.dataTitleMarkdown('Response data'));
+        data.push(markdownHelpers.dataSubtitleMarkdown('Status: ' + endpoint.responseStatus));
+        data.push(markdownHelpers.titleJsonMarkdown('Headers', endpoint.responseHeaders));
+        data.push(markdownHelpers.titleJsonMarkdown('Body', endpoint.responseBody));
       });
     });
-  }
 
-  return data.join('');
+    fs.writeFileSync('./docs/docs.md', data.join(''));
+  }
 };
-
-exports.resourceMarkdown = function (resource) {
-  return resource ? '# ' + capitalize(resource) + '\n' : '';
-}
-
-exports.endpointMarkdown = function (method, endpoint) {
-  return method && endpoint ? '## ' + method.toUpperCase() + ' ' + endpoint + '\n' : '';
-}
-
-exports.endpointDescriptionMarkdown = function (desc) {
-  return desc ? desc + '\n' : '';
-}
-
-exports.dataTitleMarkdown = function (title) {
-  return title ? '### ' + capitalize(title) + '\n' : '';
-}
-
-exports.dataSubtitleMarkdown = function (subtitle) {
-  return subtitle ? '#### ' + capitalize(subtitle) + '\n' : '';
-}
-
-exports.jsonMarkdown = function (json) {
-  return json ? '```json\n' + JSON.stringify(json, null, 2) + '\n```\n' : '';
-}
-
-exports.titleJsonMarkdown = function (title, json) {
-  if (title && Object.keys(json || {}).length) {
-    return exports.dataSubtitleMarkdown(title) + exports.jsonMarkdown(json);
-  }
-  return '';
-}
-
-function capitalize(text) {
-  return text ? text[0].toUpperCase() + text.substring(1) : '';
-}
