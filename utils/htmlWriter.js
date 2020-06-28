c5c1f5f11d8d9c5290616739551a3ce81662c908
gnardini <gonzalo.nardini.dev@gmail.com>
2020-06-28 19:31:37 -0300

    Description should be optional

--- utils/htmlWriter.js
+++ utils/htmlWriter.js
@@ -77,11 +77,13 @@ function writeEndpointHtml (endpoint) {
       'class="panel-heading" role="tab" id="heading' + cssEndpoint + '"'
     ) +
     htmlHelpers.wrapInDivWithProps(
-      htmlHelpers.wrapInDiv(
-        htmlHelpers.wrapInSubtitle('Description') +
-        htmlHelpers.wrapInSubtitleTitle(endpoint.description),
-        'container'
-      ) +
+      (endpoint.description
+        ? htmlHelpers.wrapInDiv(
+          htmlHelpers.wrapInSubtitle('Description') +
+          htmlHelpers.wrapInSubtitleTitle(endpoint.description),
+          'panel-body'
+        )
+        : '') +
       htmlHelpers.wrapInDiv(
         htmlHelpers.wrapInSubtitle('Request Data') +
         htmlHelpers.wrapInDiv(
